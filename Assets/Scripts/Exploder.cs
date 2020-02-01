dcd81cd71b060da48453b8eb0b8b2b474b7ccc7e
Sean Deneen <scdeneen@gmail.com>
2020-02-01 11:37:10 -0800

    Simplified Exploder trigger logic. Added fade out effect.

--- Assets/Scripts/Exploder.cs
+++ Assets/Scripts/Exploder.cs
@@ -5,34 +5,53 @@ using UnityEngine;
 
 public class Exploder : MonoBehaviour
 {
+    public Color fadeTo;
+
     private ParticleSystem particles;
+    private SpriteRenderer sprite;
+    private Color fadeFrom;
 
     // Start is called before the first frame update
     void Start()
     {
         particles = GetComponent<ParticleSystem>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (particles != null && particles.isPlaying)
+        {
+            var p = particles.time / particles.main.duration;
+            sprite.color = Color.Lerp(fadeFrom, fadeTo, p);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log(collider.tag);
-        if (collider.CompareTag("Bullet"))
+        if (ShouldExplode(collider))
         {
             Explode();
         }
-        else if (CompareTag("Player") && collider.CompareTag("Enemy"))
+    }
+
+    private bool ShouldExplode(Collider2D collider)
+    {
+        if (CompareTag("Player"))
         {
-            Explode();
+            return collider.CompareTag("Enemy");
         }
+        return collider.CompareTag("Bullet");
     }
 
     private void Explode()
     {
         Debug.Log("Exploded");
-        if (particles != null && !particles.isPlaying)
+        if (particles != null && sprite != null && !particles.isPlaying)
         {
             particles.Play();
+            fadeFrom = sprite.color;
         }
-        Destroy(gameObject/*, particles.main.duration*/);
+        Destroy(gameObject, particles.main.duration);
     }
 }
