1bc033899516f6ce656f7155beb45e3649e9ace8
Sean Deneen <scdeneen@gmail.com>
2020-02-02 13:05:24 -0800

    Added new health icons, changed life icon handling.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,26 +7,24 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Transform playerReference;
-    public GameObject lifePrefab;
+    public GameObject[] lifeImages;
+    public Color fadeFrom;
     public Color fadeTo;
     [Min(0.001f)]
     public float fadeTime;
 
     private PlayerBase playerStats;
-    private Stack<GameObject> remainingLives;
+    private int lifeIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
         int numLives = 0;
         if (playerReference != null && playerReference.TryGetComponent(out playerStats))
         {
             numLives = (int)playerStats.healthCurrent;
         }
         Debug.Log(String.Format("Number of Lives: {0}", numLives));
-
-        remainingLives = new Stack<GameObject>();
         AddLives(numLives);
     }
 
@@ -40,7 +38,7 @@ public class HealthBar : MonoBehaviour
         }
 
         // Update life bar state to match the game state.
-        var deltaLives = numLives - remainingLives.Count;
+        var deltaLives = numLives - lifeIndex;
         if (deltaLives > 0)
         {
             AddLives(deltaLives);
@@ -53,27 +51,22 @@ public class HealthBar : MonoBehaviour
 
     private void AddLives(int numLives = 1)
     {
-        var prevLife = (remainingLives.Count != 0) ? remainingLives.Peek() : lifePrefab;
-        for (int i = 0; i < numLives; i++)
+        StopAllCoroutines();
+        for (; lifeIndex < lifeImages.Length - numLives; lifeIndex++)
         {
-            // Place life icons adjacent to each other.
-            var nextLife = Instantiate(prevLife, transform);
-            if (prevLife.TryGetComponent(out RectTransform rectTransform))
-            {
-                // Get the size of each icon.
-                var xScale = rectTransform.sizeDelta.x * prevLife.transform.localScale.x;
-                nextLife.transform.Translate(new Vector3(xScale * i, 0));
-            }
-            remainingLives.Push(nextLife);
+            Image image = lifeImages[lifeIndex].GetComponent<Image>();
+            // Enable and set to the fadeFrom color.
+            image.gameObject.SetActive(true);
+            image.color = fadeFrom;
         }
     }
 
     private void RemoveLives(int numLives = 1)
     {
         // Fade out lost lives.
-        for (int i = 0; i < numLives; i++)
+        for (int i = lifeIndex - 1; i >= 0 && i > lifeIndex - numLives - 1; i--)
         {
-            var lastLife = remainingLives.Pop();
+            var lastLife = lifeImages[i];
             StartCoroutine(FadeOutLifeIcon(lastLife));
         }
     }
@@ -86,7 +79,6 @@ public class HealthBar : MonoBehaviour
             Debug.LogWarning("Life icon not found.");
             yield break;
         }
-        Color fadeFrom = fadeImage.color;
         float fadeDuration = 0;
 
         while (fadeImage.color != fadeTo)
